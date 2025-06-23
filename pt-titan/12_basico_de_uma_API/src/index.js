8272f8ef3e4e3e30cccb4234287a263e00a08724
Rafael Vieira <mouravieira44@gmail.com>
2025-06-23 17:28:45 -0300

    atualização

--- pt-titan/12_basico_de_uma_API/src/index.js
+++ pt-titan/12_basico_de_uma_API/src/index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
 
+//Midleware
 app.use(
     express.urlencoded({
         extends: true
@@ -10,8 +11,27 @@ app.use(
 app.use(express.json())
 
 // Rotas - Endpoint
+app.post('/criarproduto', (req, res) => {
+    const nome = req.body.name
+    const preco = req.body.price
+
+    if(!nome) {
+        res.status(422).json({
+            message: "O campo 'name' é obrigatorio"
+        })
+        return
+    }
+
+    console.log(nome)
+    console.log(preco)
+
+    res.status(201).json({
+        message: `O produto ${nome} foi enviado com sucesso!`
+    })
+})
+
 app.get('/', (req, res) => {
-    res.json({message: "Primeira rota criada, TESTANDO!!"})
+    res.status(200).json({message: "Primeira rota criada, TESTANDO!!"})
 })
 
 app.listen(3033)
